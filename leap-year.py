f08b29fe8b52007f47ef42bf709efdccbf44c360
Nargiz Nasirova <88719494+nargizraf@users.noreply.github.com>
2024-06-04 13:56:11 +0200

    commited (#1)
    Co-authored-by: 2973tok <2973tok@gmail.com>

--- leap-year.py
+++ leap-year.py
@@ -6,4 +6,6 @@ num = int(input("Enter a number: "))
 if (num % 2) == 0:
    print("{0} is Even".format(num))
 else:
-   print("{0} is Odd".format(num))  (added)
+   print("{0} is Odd".format(num))
+
+commited
